95aa74616104d45483bd35d9672e877cb85a9cb7
jzheadley <headleyjz@vcu.edu>
2019-10-19 14:33:24 -0400

    adding embeddings

--- src/metrics.py
+++ src/metrics.py
@@ -1,8 +1,13 @@
 import pandas as pd 
 import numpy as np
 
+from bert_serving.server import BertServer
+from bert_serving.server.helper import get_run_args,get_args_parser, get_shutdown_parser
 
+
+# df= pd.read_csv("wine-reviews/winemag-data-130k-v2.csv") 
 df= pd.read_csv("wine-reviews/winemag-data_first150k.csv") 
+
 
 print(df)
 print(df.columns)
@@ -10,3 +15,13 @@ wine_types = df['variety'].unique()
 # print(wine_types)
 print('There are',len(wine_types),'wine types.')
 print(df['description'].head())
+
+bert_args = get_run_args()
+print(bert_args)
+server = BertServer(bert_args)
+server.start()
+
+bc = BertClient()
+print(bc.encode(['First do it', 'then do it right', 'then do it better']))
+shut_args = get_shutdown_parser().parse_args(['-ip','localhost','-port','5555','-timeout','5000'])
+BertServer.shutdown(shut_args)
