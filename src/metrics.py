21a4af9d4db8cb3e06f46520f96799f3408ff358
jzheadley <headleyjz@vcu.edu>
2019-10-19 14:37:45 -0400

    missed an import

--- src/metrics.py
+++ src/metrics.py
@@ -1,8 +1,10 @@
 import pandas as pd 
 import numpy as np
+import time
 
 from bert_serving.server import BertServer
 from bert_serving.server.helper import get_run_args,get_args_parser, get_shutdown_parser
+from bert_serving.client import BertClient
 
 
 # df= pd.read_csv("wine-reviews/winemag-data-130k-v2.csv") 
@@ -20,8 +22,11 @@ bert_args = get_run_args()
 print(bert_args)
 server = BertServer(bert_args)
 server.start()
-
+time.sleep(20)
 bc = BertClient()
+
 print(bc.encode(['First do it', 'then do it right', 'then do it better']))
+
+
 shut_args = get_shutdown_parser().parse_args(['-ip','localhost','-port','5555','-timeout','5000'])
 BertServer.shutdown(shut_args)
