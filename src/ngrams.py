9f64e45c72d5180cbdc0099fab89e0359c5ccadf
batmaninrobin <maliknui@vcu.edu>
2019-10-19 14:24:49 -0400

    ngrams

--- src/ngrams.py
+++
@@ -1,7 +0,0 @@
-import re
-from nltk.util import ngrams
-
-s = s.lower()
-s = re.sub(r'[^a-zA-Z0-9\s]', ' ', s)
-tokens = [token for token in s.split(" ") if token != ""]
-output = list(ngrams(tokens, 3))
