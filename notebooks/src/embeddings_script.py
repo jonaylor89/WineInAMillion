459d7b45d8d1327eec71584c6f5afc5c07146011
Johannes Naylor <jonaylor89@gmail.com>
2021-12-13 22:50:46 -0500

    streamline scripts

---
+++ notebooks/src/embeddings_script.py
@@ -0,0 +1,100 @@
+import argparse
+import logging
+import requests
+
+import boto3
+import os
+import json
+import io
+import time
+import torch
+import pandas as pd
+from sentence_transformers import models, losses, SentenceTransformer
+
+logger = logging.getLogger(__name__)
+logger.setLevel(logging.DEBUG)
+
+# set the constants for the content types
+CONTENT_TYPE = "application/json"
+
+
+def model_fn(model_dir):
+    logger.info("model_fn")
+    device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+    logger.info(model_dir)
+    model = SentenceTransformer(model_dir + "/transformer/")
+    logger.info(model)
+    return model.to(device)
+
+
+# Deserialize the Invoke request body into an object we can perform prediction on
+def input_fn(serialized_input_data, content_type=CONTENT_TYPE):
+    logger.info("Deserializing the input data.")
+    print(serialized_input_data)
+    if content_type == CONTENT_TYPE:
+        data = json.loads(serialized_input_data)
+        return data
+    raise Exception(
+        "Requested unsupported ContentType in content_type: {}".format(content_type)
+    )
+
+
+# Perform prediction on the deserialized object, with the loaded model
+def predict_fn(input_object, model):
+    logger.info("Calling model")
+    start_time = time.time()
+    print(input_object)
+    sentenceToEncode = input_object["data"]
+    print("encoding", sentenceToEncode)
+    sentence_embeddings = model.encode(sentenceToEncode)
+    print("--- Inference time: %s seconds ---" % (time.time() - start_time))
+    print(sentence_embeddings.shape)
+    response = sentence_embeddings.tolist()
+    return response
+
+
+# Serialize the prediction result into the desired response content type
+def output_fn(prediction, accept):
+    logger.info("Serializing the generated output.")
+    if accept == "application/json":
+        output = json.dumps({"embeddings": prediction})
+        return output
+    raise Exception(
+        "Requested unsupported ContentType in Accept: {}".format(content_type)
+    )
+
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser()
+
+    # Sagemaker specific arguments. Defaults are set in the environment variables.
+    parser.add_argument("--output-data-dir", type=str)
+    parser.add_argument("--model-dir", type=str, default=os.environ["SM_MODEL_DIR"])
+    parser.add_argument("--train", type=str)
+
+    args = parser.parse_args()
+
+    model_name = 'sentence-transformers/all-MiniLM-L6-v2'
+
+    saved_model_dir = 'transformer'
+    if not os.path.isdir(saved_model_dir):
+        os.makedirs(saved_model_dir)
+
+    model = SentenceTransformer(model_name)
+    model.save(args.model_dir)
+
+    # Load the training data into a Pandas dataframe and make sure it is in the appropriate format
+    raw_data = pd.read_csv(args.train)
+
+    embeddings = []
+    for i in range(len(raw_data["clean_desc"]) - 100, len(df["clean_desc"])):
+        vector = model.encode([raw_data["clean_desc"][i]])
+        embeddings.append(vector)
+
+    embeddings_flattened = list(map(lambda x: x[0], embeddings))
+    embeddings_df = pd.DataFrame(embeddings_flattened)
+    embeddings_df = embeddings_df[:-1]
+    del embeddings_df["embeddings.csv"]
+
+    # Save to output data dir
+    embeddings_df.to_csv(f"{args.output_data_dir}/embeddings.csv")
---
+++ notebooks/src/nn_script.py
@@ -0,0 +1,117 @@
+import argparse
+import logging
+import requests
+
+import boto3
+import os
+import json
+import io
+import time
+import pandas as pd
+import joblib
+from sklearn.neighbors import NearestNeighbors
+import numpy as np
+
+logger = logging.getLogger(__name__)
+logger.setLevel(logging.DEBUG)
+
+CONTENT_TYPE = "application/json"
+
+
+def model_fn(model_dir):
+    logger.info("model_fn")
+    logger.info(model_dir)
+    model = joblib.load(model_dir + "/model.joblib")
+    logger.info(model)
+    return model
+
+
+# Deserialize the Invoke request body into an object we can perform prediction on
+def input_fn(serialized_input_data, content_type=CONTENT_TYPE):
+    logger.info("Deserializing the input data.")
+    if content_type == CONTENT_TYPE:
+        data = json.loads(serialized_input_data)
+        return data
+    raise Exception(
+        "Requested unsupported ContentType in content_type: {}".format(content_type)
+    )
+
+
+# Perform prediction on the deserialized object, with the loaded model
+def predict_fn(input_object, model):
+    logger.info("Calling model")
+    start_time = time.time()
+    print(input_object)
+
+    try:
+        embeddingsVector = [input_object["embeddings"]]
+
+        kneighbors = 5
+        if "kneighbors" in input_object.keys():
+            kneighbors = input_object["kneighbors"]
+
+        print(f"k neighbors {kneighbors}")
+        distanceNeighbors = model.kneighbors(
+            embeddingsVector, kneighbors, return_distance=True
+        )
+        print("--- Inference time: %s seconds ---" % (time.time() - start_time))
+        print(f"distanceNeighbors {distanceNeighbors}")
+        zipped = list(
+            zip(distanceNeighbors[1].tolist()[0], distanceNeighbors[0].tolist()[0])
+        )
+        print(f"zipped neighbors {zipped}")
+        return zipped
+
+    except Exception as e:
+        print(e)
+        return []
+
+
+# Serialize the prediction result into the desired response content type
+def output_fn(prediction, accept):
+    logger.info("Serializing the generated output.")
+    if accept == "application/json":
+        output = json.dumps({"recommendations": prediction})
+        return output
+    raise Exception(
+        "Requested unsupported ContentType in Accept: {}".format(content_type)
+    )
+
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser()
+
+    # Hyperparameters are described here.
+    parser.add_argument("--n_neighbors", type=int, default=10)
+    parser.add_argument("--metric", type=str, default="cosine")
+
+    # Sagemaker specific arguments. Defaults are set in the environment variables.
+    parser.add_argument("--output-data-dir", type=str)
+    parser.add_argument("--model-dir", type=str, default=os.environ["SM_MODEL_DIR"])
+    parser.add_argument("--train", type=str)
+
+    args = parser.parse_args()
+
+    # Load the training data into a Pandas dataframe and make sure it is in the appropriate format
+    raw_data = pd.read_csv(args.train)
+
+    def convert_to_list(raw_review_vec):
+        review_vec_trimmed = raw_review_vec.replace("[", "").replace("]", "")
+        review_vec = np.fromstring(review_vec_trimmed, dtype=float, sep="  ")
+        review_vec_list = review_vec.tolist()
+        return review_vec_list
+
+    raw_data["review_vec"] = raw_data["review_vector"].apply(convert_to_list)
+    wine_vectors_list = np.array(list(raw_data["review_vec"]))
+
+    # Supply the hyperparameters of the nearest neighbors model
+    n_neighbors = args.n_neighbors
+    metric = args.metric
+
+    # Now, fit the nearest neighbors model
+    nn = NearestNeighbors(n_neighbors=n_neighbors, metric=metric)
+    model_nn = nn.fit(wine_vectors_list)
+    print("model has been fitted")
+
+    # Save the model to the output location in S3
+    joblib.dump(model_nn, os.path.join(args.model_dir, "model.joblib"))
